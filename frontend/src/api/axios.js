5e03a506efd8672c0b658374018ea20eaffbd23e
AarisPFW <aleym01@pfw.edu>
2024-12-10 15:36:08 -0500

    BASE_URL fix for testing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,11 @@
 // src/api/axios.js
 import axios from 'axios';
 
+//Before
+// const BASE_URL = 'http://localhost:8000'; 
+
+//After
+// Comment this for testing
 const BASE_URL = 'http://127.0.0.1:8000'; 
 
 const axiosInstance = axios.create({
